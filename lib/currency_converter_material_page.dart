9c9dc435b0370bff73ec946a9e7e0b379e6b8a1f
Abhinav Singh <abhinavsinghabhi56@gmail.com>
2024-03-30 04:06:18 +0530

    final UI

--- lib/currency_converter_material_page.dart
+++ lib/currency_converter_material_page.dart
@@ -4,40 +4,72 @@ class CurrencyConverterMaterialPage extends StatelessWidget {
   const CurrencyConverterMaterialPage({super.key});
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    final border = OutlineInputBorder(
+      borderSide: const BorderSide(
+        width: 2.5,
+      ),
+      borderRadius: BorderRadius.circular(10),
+    );
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          "Currency Converter",
+          style: TextStyle(fontWeight: FontWeight.w600),
+        ),
+        backgroundColor: Colors.blueGrey,
+        foregroundColor: Colors.white,
+      ),
       backgroundColor: Colors.blueGrey,
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Text(
+            const Text(
               '0',
               style: TextStyle(
-                fontSize: 55,
-                fontWeight: FontWeight.bold,
-                color: Colors.white
-              ),
+                  fontSize: 55,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white),
             ),
-            TextField(
-              style: TextStyle(
-                color: Colors.black
-              ),
-              decoration: InputDecoration(
-                hintText: 'Please Enter The Amount In USD',
-                hintStyle: TextStyle(
-                  color: Colors.black
-                ),
-                prefixIcon: Icon(Icons.monetization_on_outlined, color: Colors.black,),
-                filled: true,
-                fillColor: Colors.white,
-                focusedBorder: OutlineInputBorder(
-                  borderSide: BorderSide(
-                    width: 2,
-                    strokeAlign: BorderSide.strokeAlignInside
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 10),
+              child: TextField(
+                style: const TextStyle(color: Colors.black),
+                decoration: InputDecoration(
+                  hintText: 'Please Enter The Amount In USD',
+                  hintStyle: const TextStyle(color: Colors.black),
+                  prefixIcon: const Icon(
+                    Icons.monetization_on_outlined,
+                    color: Colors.black,
                   ),
-                )
+                  filled: true,
+                  fillColor: Colors.white,
+                  focusedBorder: border,
+                  enabledBorder: border,
+                ),
+                keyboardType:
+                    const TextInputType.numberWithOptions(decimal: true),
               ),
             ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 10),
+              child: ElevatedButton(
+                onPressed: () {},
+                style: TextButton.styleFrom(
+                  backgroundColor: Colors.black,
+                  foregroundColor: Colors.white,
+                  elevation: 5,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(8),
+                  ),
+                  minimumSize: const Size(double.infinity, 50),
+                ),
+                child: const Text(
+                  "Convert",
+                  style: TextStyle(fontSize: 16),
+                ),
+              ),
+            )
           ],
         ),
       ),
